3277d755a83e79111d637f24c0bb63ed26dcc7f8
ashishreddyaleti <35349499+ashishreddyaleti@users.noreply.github.com>
2022-02-28 15:58:59 -0600

    Update sqlbuild.ps1

--- sqlbuild.ps1
+++ sqlbuild.ps1
@@ -1,4 +1,4 @@
 set-location -Path "C:\Users\hammerdb\HammerDB-4.3\"
 Write-Host "Workload Starting"
 Write "Starting Build"
-.\hammerdbcli.bat auto C:\Users\hammerdb\HammerDB-4.3\sqlbuild.tcl
+.\hammerdbcli.bat auto sqlbuild.tcl
